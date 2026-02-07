9ebf8d4b34d3803dc5c7073347e30d25d8f07ae7
Nimesh Prajapati <nimprajapati2004@gmail.com>
2026-02-07 23:32:03 +0530

    Create Add product functionality

--- webApp/src/main/java/com/spring/webApp/controller/ProductController.java
+++ webApp/src/main/java/com/spring/webApp/controller/ProductController.java
@@ -6,23 +6,26 @@ package com.spring.webApp.controller;
 import com.spring.webApp.model.Product;
 import com.spring.webApp.service.productService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
 public class ProductController {
     @Autowired
     productService service;
-    @RequestMapping("/products")
+    @GetMapping("/products")
      public List<Product> getProdaucts(){
         System.out.println(" from all products controller");
          return service.getAllProducts();
      }
 
-    @RequestMapping("/product/{id}")
+    @GetMapping("/product/{id}")
     public Product getProduct(@PathVariable int id) {
         return service.getOneProduct(id);
+    }
+    @PostMapping("/products")
+    public void addProduct( @RequestBody Product prod) {
+//        System.out.println(" added product");
+        service.addProduct(prod);
     }
 }
--- webApp/src/main/java/com/spring/webApp/service/productService.java
+++ webApp/src/main/java/com/spring/webApp/service/productService.java
@@ -3,21 +3,28 @@ package com.spring.webApp.service;
 import com.spring.webApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 @Service
 public class productService {
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
                 new Product(101, "Redmi", 30000),
                 new Product(102, "Google", 80000),
-                new Product(103, "BlackBerry", 70000));
+                new Product(103, "BlackBerry", 70000)));
 
+    //Get All Products
     public List<Product> getAllProducts() {
-//        System.out.println("getproducts...Hello" + products.size());
         return products;
     }
-
+    // Get product By ID
     public Product getOneProduct(int id) {
-        return products.stream().filter(p ->p.getPId() == id ).findFirst().orElseThrow();
+        return products.stream().filter(p -> p.getPId() == id).findFirst().orElse(new  Product(0, "No Product Found", 0));
+    }
+
+    // Add product
+    public void addProduct(Product prod) {
+         products.add(prod);
     }
 }
