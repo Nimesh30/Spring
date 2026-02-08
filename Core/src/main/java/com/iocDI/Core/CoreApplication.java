eb41cbddc7c23798ab93722273fc4c331e14a271
Nimesh Prajapati <nimprajapati2004@gmail.com>
2026-02-08 23:26:19 +0530

    .

--- Core/src/main/java/com/iocDI/Core/CoreApplication.java
+++ Core/src/main/java/com/iocDI/Core/CoreApplication.java
@@ -18,4 +18,4 @@ public class CoreApplication {
 		car.setAge(18);
 		System.out.println(car.getAge());
 	}
-}
+} 
--- Core/src/main/java/com/iocDI/Core/Engine.java
+++ Core/src/main/java/com/iocDI/Core/Engine.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Component;
 
 //@Component // This is Spring Boot - Annotations
 public class Engine {
-    public Engine(){
+    public Engine() {
         System.out.println("Engine Constructor...");
     }
+
     public void start() {
         System.out.println("Engine start");
     }
