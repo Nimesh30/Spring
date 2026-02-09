ec05136f52262168c39695365dfa2cc890de8919
Nimesh Prajapati <nimprajapati2004@gmail.com>
2026-02-09 19:03:37 +0530

    react files for frontend

---
+++ StudentApp/Frontend/studentApp/src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import StudentList from "./components/StudentList";
+import StudentForm from "./components/StudentForm";
+
+function App() {
+  return (
+    <div>
+      <h1>Student Management System</h1>
+
+      <Routes>
+        <Route path="/" element={<StudentList />} />
+        <Route path="/add-student" element={<StudentForm />} />
+        <Route path="/edit-student/:id" element={<StudentForm />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
