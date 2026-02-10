6bc3e3e82d9576d4072899184835ccff76ef83d2
Nimesh Prajapati <nimprajapati2004@gmail.com>
2026-02-10 23:26:54 +0530

    Improve existing functionality

--- StudentApp/Frontend/studentApp/src/App.jsx
+++ StudentApp/Frontend/studentApp/src/App.jsx
@@ -5,7 +5,7 @@ import StudentForm from "./components/StudentForm";
 function App() {
   return (
     <div>
-      <h1>Student Management System</h1>
+      <h1 align="center">Student Management System</h1>
 
       <Routes>
         <Route path="/" element={<StudentList />} />
