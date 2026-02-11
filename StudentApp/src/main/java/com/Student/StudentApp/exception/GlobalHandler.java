cb5fc9b680eb1b3f83092a1ce0f8d4d89c818a7b
Nimesh Prajapati <nimprajapati2004@gmail.com>
2026-02-11 18:20:19 +0530

    Create global exception handler

--- StudentApp/src/main/java/com/Student/StudentApp/exception/GlobalHandler.java
+++ StudentApp/src/main/java/com/Student/StudentApp/exception/GlobalHandler.java
@@ -1,4 +1,15 @@
 package com.Student.StudentApp.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDuplicateEmail() {
+        return ResponseEntity.badRequest().body("Email already exists");
+    }
 }
