3ad48fef0f005b9989991b6fd61540520815dc72
Nimesh Prajapati <nimprajapati2004@gmail.com>
2026-02-11 18:22:17 +0530

    add JPA method instead of query

--- StudentApp/src/main/java/com/Student/StudentApp/repository/StudentRepo.java
+++ StudentApp/src/main/java/com/Student/StudentApp/repository/StudentRepo.java
@@ -4,8 +4,15 @@ import com.Student.StudentApp.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Integer> {
+
+
+    List<Student> findByIsDeletedFalse();
+
+
 
 
 }
