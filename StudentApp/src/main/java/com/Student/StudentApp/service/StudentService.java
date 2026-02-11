a4cc5b78b1eeb64553dd429de43996d694b4556e
Nimesh Prajapati <nimprajapati2004@gmail.com>
2026-02-11 18:23:24 +0530

    Implement soft delete func.

--- StudentApp/src/main/java/com/Student/StudentApp/service/StudentService.java
+++ StudentApp/src/main/java/com/Student/StudentApp/service/StudentService.java
@@ -14,8 +14,16 @@ public class StudentService {
 
     @Autowired
     StudentRepo studentRepo;
+
+    // This method retrieves all student records from the database using the findAll() method
+    // provided by the JpaRepository interface.
+//    public List<Student> getAllStudents() {
+//        return studentRepo.findAll();
+//    }
+    //This will fetch only those students whose isDeleted is false,
+    //effectively hiding the "deleted" records without actually removing them from the database.
     public List<Student> getAllStudents() {
-        return studentRepo.findAll();
+        return studentRepo.findByIsDeletedFalse();
     }
 
     public Student getOneStudent( int id) {
@@ -25,13 +33,46 @@ public class StudentService {
     public void addStudent(Student student) {
         studentRepo.save(student);
     }
+// This method is removing the record from the database.
+//    public void deleteStudent(int id) {
+//        studentRepo.deleteById(id);
+//    }
+
+
 
     public void deleteStudent(int id) {
-        studentRepo.deleteById(id);
+
+        Student student = studentRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Student not found"));
+
+        student.setDeleted(true);   // mark as deleted
+        studentRepo.save(student);    // update record instead of deleting
     }
 
+
+//    public void updateStudent(int id, Student student) {
+//        student.setId(id);
+//        studentRepo.save(student);
+//    }
+
+
     public void updateStudent(int id, Student student) {
-        student.setId(id);
-        studentRepo.save(student);
+
+        Student existing = studentRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Student not found"));
+
+        existing.setFirstName(student.getFirstName());
+        existing.setLastName(student.getLastName());
+        existing.setEmail(student.getEmail());
+        existing.setDob(student.getDob());
+        existing.setGender(student.getGender());
+        existing.setAdmissionDate(student.getAdmissionDate());
+        existing.setClassName(student.getClassName());
+        existing.setCity(student.getCity());
+        existing.setStateCode(student.getStateCode());
+        existing.setCountry(student.getCountry());
+
+        studentRepo.save(existing);
     }
+
 }
